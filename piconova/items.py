f85ddca0f708665cac23201c98918b6e8157fe82
Stella Cannefax <stella.cannefax@gmail.com>
2014-03-07 11:22:54 -0600

    Update items.py

--- piconova/items.py
+++ piconova/items.py
@@ -1,12 +1,14 @@
 from scrapy.item import Item, Field
 
 class Torrent(Item):
-	title   = Field()
-	torrent = Field()
-	magnet  = Field()
-	updated = Field()
-	added   = Field()		
-	seeds   = Field()
-	size    = Field()
+	title    = Field()
+	torrent  = Field()
+	magnet   = Field()
+	updated  = Field()
+	added    = Field()		
+	seeds    = Field()
+	leeches  = Field()
+	size     = Field()
+	uploader = Field()
 	visit_id = Field()
 	visit_status = Field()
