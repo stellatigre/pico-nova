22a2ce06c780f05f84c5590db8368e9803f6d0c3
Stella Cannefax <stella.cannefax@gmail.com>
2014-03-10 08:22:42 -0500

    Update picobay.py

--- piconova/spiders/picobay.py
+++ piconova/spiders/picobay.py
@@ -18,21 +18,21 @@ class PirateSpider(PicoSpider):
 	xpath_dict = {
 		'title'    : ('//*[@id="title"]'),
 		'seeds'    : ('//*[@id="details"]/dl[1]/dd[7]/text()', 
-			      	  '//*[@id="details"]/dl[2]/dd[3]/text()',
-			      	  '//*[@id="details"]/dl[1]/dd[8]//text()'),
+			      '//*[@id="details"]/dl[2]/dd[3]/text()',
+			      '//*[@id="details"]/dl[1]/dd[8]//text()'),
 		'magnet'   : ('//*[@id="details"]/div[3]/a[1]/@href',
-			      	  '//*[@id="details"]/div[4]/a/@href',
-	                  '/html/body/div[3]/div[2]/div/div/div/div[2]/div[10]/a/@href'),
+			      '//*[@id="details"]/div[4]/a/@href',
+	                      '/html/body/div[3]/div[2]/div/div/div/div[2]/div[10]/a/@href'),
 		'torrent'  : ('//*[@id="details"]/div[4]/a[2]/@href',
-			      	  '//*[@id="details"]/div[3]/a[2]/@href'),
+			      '//*[@id="details"]/div[3]/a[2]/@href'),
 		'category' : ('//*[@id="details"]/dl[1]/dd[1]/a'),
 		'leech'    : ('//*[@id="details"]/dl[1]/dd[9]/text()',
-			      	  '//*[@id="details"]/dl[2]/dd[4]/text()'),
+			      '//*[@id="details"]/dl[2]/dd[4]/text()'),
 		'size'     : ('//*[@id="details"]/dl[1]/dd[3]/text()',),
 		'added'    : ('//*[@id="details"]/dl[1]/dd[5]/text()',
-			      	  '//*[@id="details"]/dl[2]/dd[1]/text()'),
+			      '//*[@id="details"]/dl[2]/dd[1]/text()'),
 		'uploader' : ('//*[@id="details"]/dl[1]/dd[6]/a/text()',
-			      	  '//*[@id="details"]/dl[2]/dd[2]/a/text()')
+			      '//*[@id="details"]/dl[2]/dd[2]/a/text()')
 	}		
 					 															
 	rules = (
