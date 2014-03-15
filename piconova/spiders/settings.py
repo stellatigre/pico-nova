a99866dc06173229f2e777fca1e92172282bd443
Stella Cannefax <stella.cannefax@gmail.com>
2014-03-15 14:04:08 -0500

    Update settings.py

--- piconova/spiders/settings.py
+++ piconova/spiders/settings.py
@@ -1,12 +1,12 @@
 BOT_NAME = 'piconova'
 CONCURRENT_REQUESTS = 420
-COOKIES_ENABLED = False
+COOKIES_ENABLED = True
 SPIDER_MODULES = ['piconova.spiders']
 NEWSPIDER_MODULE = 'piconova.spiders'
 
 DOWNLOADER_MIDDLEWARES = {
     'piconova.random_user_agent.RandomUserAgentMiddleware': 400,
-    'scrapy.contrib.downloadermiddleware.useragent.UserAgentMiddleware': None, }
+}
 
 USER_AGENT_LIST = (
     'Mozilla/5.0 (Windows; U; Windows NT 5.1; de; rv:1.9.2.3) Gecko/20100401 Firefox/3.6.3',
@@ -16,9 +16,5 @@ USER_AGENT_LIST = (
     'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_6_8) AppleWebKit/537.13+ (KHTML, like Gecko) Version/5.1.7 Safari/534.57.2',
     'Mozilla/5.0 (iPad; CPU OS 6_0 like Mac OS X) AppleWebKit/536.26 (KHTML, like Gecko) Version/6.0 Mobile/10A5355d Safari/8536.25',
     'Mozilla/5.0 (Windows; U; MSIE 9.0; WIndows NT 9.0; en-US)',
-    'Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Win64; x64; Trident/5.0')
-
-DENY_RULES_MINI = ('')
-
-DENY_RULES_KATT = ('/search/*', 'utorrent.btsearch',
-                   '/blog/*', '/user/*', '/community/*', '/comments/*', '/show/*')
+    'Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Win64; x64; Trident/5.0'
+)
